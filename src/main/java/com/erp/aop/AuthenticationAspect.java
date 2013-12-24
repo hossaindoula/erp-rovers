b442b35195fc0b49bbc52ef26d1f5615e8213c25
hossaindoula <hossaindoula@gmail.com>
2013-12-24 15:48:17 +0000

    AOP added...

---
+++ src/main/java/com/erp/aop/AuthenticationAspect.java
@@ -0,0 +1,45 @@
+package com.erp.aop;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.erp.security.model.User;
+import com.erp.util.ERPConstants;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.erp.security.SessionUtil;
+
+@Aspect
+public class AuthenticationAspect {
+	
+	@Pointcut("within(@org.springframework.stereotype.Controller *) " +
+			"&& !within(@org.springframework.stereotype.Controller com.erp.guest.AuthenticationController) ")
+	public void allControllers(){}
+	
+	@Pointcut("execution(* *(..))")
+    public void methodPointcut() {}
+
+	@Around("allControllers() && methodPointcut()")
+	public Object proceedToAction(ProceedingJoinPoint proceedJoinPoint) throws Throwable {
+		if(SessionUtil.getSession().getAttribute(ERPConstants.SESSION_USER) instanceof User){
+            return proceedJoinPoint.proceed();
+		} else {
+            return new ModelAndView("redirect:/");
+		}
+	}
+
+}
