696b3a5e86cc7f363956b1639ffebb5fbf671235
hossaindoula <hossaindoula@gmail.com>
2013-12-24 16:05:16 +0000

    g

---
+++ src/main/java/com/erp/crm/CustomerController.java
@@ -0,0 +1,17 @@
+package com.erp.crm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hossaindoula
+ * Date: 7/18/12
+ * Time: 4:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+@RequestMapping("/customer/")
+public class CustomerController {
+}
---
+++ src/main/java/com/erp/crm/InvoiceController.java
@@ -0,0 +1,17 @@
+package com.erp.crm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 7/18/12
+ * Time: 4:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+@RequestMapping("/invoice/")
+public class InvoiceController {
+}
