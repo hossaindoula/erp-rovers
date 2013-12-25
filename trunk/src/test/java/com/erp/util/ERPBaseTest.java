a946c5fcaf7b9051b36f8fc1946a6719925f43df
hossaindoula@gmail.com <hossaindoula@gmail.com@81cef9fa-1806-5434-b0b5-047936f3cc04>
2013-12-25 18:50:20 +0000

    test...

--- trunk/src/test/java/com/erp/util/ERPBaseTest.java
+++ trunk/src/test/java/com/erp/util/ERPBaseTest.java
@@ -10,4 +10,17 @@ import junit.framework.TestCase;
  * To change this template use File | Settings | File Templates.
  */
 public class ERPBaseTest extends TestCase {
+    protected int value1, value2;
+
+    // assigning the values
+    protected void setUp(){
+        value1 = 3;
+        value2 = 3;
+    }
+
+    // test method to add two values
+    public void testAdd(){
+        double result= value1 + value2;
+        assertTrue(result == 6);
+    }
 }
